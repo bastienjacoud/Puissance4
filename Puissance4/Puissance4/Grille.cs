f105eecbe7844c6db4ea3eac1d3c2d24ac0b799c
Lombardo_Florian <lombardoflorian@gmail.com>
2017-02-04 19:01:53 +0100

    85956956 IF

--- Puissance4/Puissance4/Grille.cs
+++ Puissance4/Puissance4/Grille.cs
@@ -87,13 +87,13 @@ namespace Puissance4
             return i;
         }
 
-        public Boolean verifHorizontale()
+        public int verifHorizontale()                                           //0 si personne gagne, 1 si jaune, 2 si rouge
         {
             Boolean gagne = true;                                               //true si partie gagnée, false sinon
             int numJ;
             for (int i_ligne = 0; i_ligne < 6; i_ligne++)                       //parcourt les lignes
             {
-                for (int i_col = 0; i_col < 4; i_col++)                         //parcout les colonnes
+                for (int i_col = 0; i_col < 4; i_col++)                         //parcourt les colonnes
                 {
                     gagne = true;                                               //temporairement à true
                     if (_map[i_ligne, i_col] != null)                           //si on tombe sur un pion
@@ -108,15 +108,19 @@ namespace Puissance4
                         }
                         if (gagne)                                              //si 4 pions alignés alors on renvoie true
                         {
-                            return gagne;
+                            return numJ;
                         }
+                    }
+                    else
+                    {
+                        gagne = false;
                     }
                 }
             }
-            return gagne;
+            return 0;
         }
 
-        public Boolean verifVerticale(){
+        public int verifVerticale(){                                            //0 si personne gagne, 1 si jaune, 2 si rouge
             Boolean gagne = true;                                               //true si partie gagnée, false sinon
             int numJ;
             for (int i_col = 0; i_col < 6; i_col++)                             //parcourt les colonnes
@@ -136,19 +140,213 @@ namespace Puissance4
                         }
                         if (gagne)
                         {
-                            return gagne;
+                            return numJ;
                         }
+                    }
+                    else
+                    {
+                        gagne = false;
                     }
                 }
             }
-            return gagne;
+            return 0;
         }
-        /*
-        public Boolean verifDiagonale()
+
+        public int verifDiagonale()                                             //0 si personne gagne, 1 si jaune, 2 si rouge
         {
-            Boolean gagne = true;
-            int numJ;
+            Boolean gagneDiag = true;                                           //true si une des diagonales donne la victoire
+            Boolean gagneDiagHG = true;
+            Boolean gagneDiagHD = true;
+            Boolean gagneDiagBD = true;
+            Boolean gagneDiagBG = true;
+            Boolean gagneDiag1 = true;                                          //concerne les  diagonales
+            Boolean gagneDiag2 = true;
+            Boolean gagneDiag3 = true;
+            Boolean gagneDiag4 = true;                                          //hors du petit carré
+            int numJ = 0;
+            for (int i_ligne = 0; i_ligne < 4; i_ligne++)                       //parcourt les lignes du petit carré à vérifier
+            {
+                for (int i_col = 0; i_col < 4; i_col++)                         //parcourt les colonnes du petit carré à vérifier
+                {
+                    gagneDiag = true;
+                    gagneDiagBD = true;
+                    gagneDiagBG = true;
+                    gagneDiagHD = true;
+                    gagneDiagHG = true;
+                    if (_map[i_ligne, i_col] != null)                           //si on rencontre un jeton
+                    {
+                        numJ = _map[i_ligne, i_col].pion.numJ;
+                        for (int diag = 0; diag < 3; diag++)                    //parcourt les jetons sur les diagonales du jeton concerné
+                        {
+                            if ((i_ligne - 3) >= 0 && (i_col - 3) >= 0)                             //si on peut aligner 4 (autres) jetons en haut à gauche du jeton de base
+                            {
+                                if (_map[i_ligne - diag, i_ligne - diag] != null)                   //si on rencontre un jeton
+                                {
+                                    if (_map[i_ligne - diag, i_col - diag].pion.numJ != numJ)       //si le jeton rencontré est de la même couleur que celui de base
+                                    {
+                                        gagneDiagHG = false;
+                                    }
+                                }
+                                else
+                                {
+                                    gagneDiagHG = false;
+                                }
+                            }
+                            if ((i_ligne - 3) >= 0 && (i_col + 3) <= 6)                             //si on peut aligner 4 (autres) jetons en haut à droite du jeton de base
+                            {
+                                if (_map[i_ligne - diag, i_col + diag] != null)                   //si on rencontre un jeton
+                                {
+                                    if (_map[i_ligne - diag, i_col + diag].pion.numJ != numJ)       //si le jeton rencontré est de la même couleur que celui de base
+                                    {
+                                        gagneDiagHD = false;
+                                    }
+                                }
+                                else
+                                {
+                                    gagneDiagHD = false;
+                                }
+                            }
+                            if ((i_ligne + 3) <= 6 && (i_col + 3) <= 6)                             //si on peut aligner 4 (autres) jetons en bas à droite du jeton de base
+                            {
+                                if (_map[i_ligne + diag, i_col + diag] != null)                     //si on rencontre un jeton
+                                {
+                                    if (_map[i_ligne + diag, i_col + diag].pion.numJ != numJ)       //si le jeton rencontré est de la même couleur que celui de base
+                                    {
+                                        gagneDiagBD = false;
+                                    }
+                                }
+                                else
+                                {
+                                    gagneDiagBD = false;
+                                }
+                            }
+                            if ((i_ligne + 3) >= 6 && (i_col - 3) <= 0)                             //si on peut aligner 4 (autres) jetons en bas à gauche du jeton de base
+                            {
+                                if (_map[i_ligne + diag, i_col - diag] != null)                     //si on rencontre un jeton
+                                {
+                                    if (_map[i_ligne + diag, i_col - diag].pion.numJ != numJ)       //si le jeton rencontré est de la même couleur que celui de base
+                                    {
+                                        gagneDiagBG = false;
+                                    }
+                                }
+                                else
+                                {
+                                    gagneDiagBG = false;
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            if (gagneDiagHG || gagneDiagHD || gagneDiagBG || gagneDiagBD)               //si une des diagonales du petit carré est valide
+            {
+                return numJ;
+            }
+            //IL RESTE 4 DIAGONALES HORS DU PETIT CARRE A TESTER
+            if (_map[4, 1] != null)                                         //première diagonale
+            {
+                numJ = _map[4, 1].pion.numJ;
+                for (int diag1 = 0; diag1 < 3; diag1++)
+                {
+                    if (_map[4 - diag1, 1 + diag1].pion.numJ != numJ)
+                    {
+                        gagneDiag1 = false;
+                    }
+                }
+                if (gagneDiag1)
+                {
+                    return numJ;
+                }
+            }
+
+            if (_map[5, 1] != null)                                         //deuxième diago
+            {
+                numJ = _map[5, 1].pion.numJ;
+                for (int diag2 = 0; diag2 < 3; diag2++)
+                {
+                    if (_map[5 - diag2, 1 + diag2].pion.numJ != numJ)
+                    {
+                        gagneDiag2 = false;
+                    }
+                }
+                if (gagneDiag2)
+                {
+                    return numJ;
+                }
+                else
+                {
+                    gagneDiag2 = true;
+                }
+            }
+            if (_map[4, 2] != null)                                         //deuxième diagonale
+            {
+                numJ = _map[4, 2].pion.numJ;
+                for (int diag2 = 0; diag2 < 3; diag2++)
+                {
+                    if (_map[4 - diag2, 2 + diag2].pion.numJ != numJ)
+                    {
+                        gagneDiag2 = false;
+                    }
+                }
+                if (gagneDiag2)
+                {
+                    return numJ;
+                }
+            }
+
+
+            if (_map[5, 2] != null)                                         //troisième diagonale
+            {
+                numJ = _map[5, 2].pion.numJ;
+                for (int diag3 = 0; diag3 < 3; diag3++)
+                {
+                    if (_map[5 - diag3, 2 + diag3].pion.numJ != numJ)
+                    {
+                        gagneDiag3 = false;
+                    }
+                }
+                if (gagneDiag3)
+                {
+                    return numJ;
+                }
+                else
+                {
+                    gagneDiag3 = true;
+                }
+            }
+            if (_map[4, 3] != null)                                         //troisième diagonale
+            {
+                numJ = _map[4, 3].pion.numJ;
+                for (int diag3 = 0; diag3 < 3; diag3++)
+                {
+                    if (_map[4 - diag3, 3 + diag3].pion.numJ != numJ)
+                    {
+                        gagneDiag3 = false;
+                    }
+                }
+                if (gagneDiag3)
+                {
+                    return numJ;
+                }
+            }
+
+            if (_map[5, 3] != null)
+            {
+                numJ = _map[5, 3].pion.numJ;
+                for (int diag4 = 0; diag4 < 3; diag4++)
+                {
+                    if (_map[5 - diag4, 3 + diag4].pion.numJ != numJ)
+                    {
+                        gagneDiag4 = false;
+                    }
+                }
+                if (gagneDiag4)
+                {
+                    return numJ;
+                }
+            }
+            return 0;
         }
-        */
     }
 }
